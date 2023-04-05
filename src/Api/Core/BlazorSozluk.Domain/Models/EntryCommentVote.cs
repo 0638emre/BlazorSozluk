41fa3d4bea8ac21b9f1112932346d7edf243d203
Emre COŞKUN <emrecoskun06@hotmail.com>
2023-04-06 00:17:56 +0300

    Entityler modeller oluşturuldu.

---
+++ src/Api/Core/BlazorSozluk.Domain/Models/EntryCommentVote.cs
@@ -0,0 +1,12 @@
+﻿using BlazorSozluk.Common.ViewModels;
+
+namespace BlazorSozluk.Domain.Models
+{
+    public class EntryCommentVote : BaseEntity
+    {
+        public Guid EntryCommentVoteId { get; set; }
+        public VoteTypes VoteType { get; set; }
+        public Guid CreatedById { get; set; }
+        public virtual EntryComment EntryComment {get; set; }
+    }
+}
---
+++ src/Common/BlazorSozluk.Common/ViewModels/Enums.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorSozluk.Common.ViewModels
+{
+    public enum VoteTypes
+    {
+        /// <summary>
+        /// oy yok ise
+        /// </summary>
+        None = -1,
+
+        /// <summary>
+        /// oy olumsuz ise
+        /// </summary>
+        DownVote = 0,
+
+        /// <summary>
+        /// oy olumlu ise
+        /// </summary>
+        UpVote = 1
+    }
+}
